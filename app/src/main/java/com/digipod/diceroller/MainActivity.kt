ef243e33739a70f368c6192281d5eb5c41abdad1
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-13 15:03:01 +0530

    removed some comments

--- app/src/main/java/com/digipod/diceroller/MainActivity.kt
+++ app/src/main/java/com/digipod/diceroller/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.digipod.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,19 +13,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn = findViewById(R.id.btnRoll)  // initialize btn
-        imgDice = findViewById(R.id.imgDice)  // initialize textDice
+        btn = findViewById(R.id.btnRoll)
+        imgDice = findViewById(R.id.imgDice)
         btn.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
         val dice = Dice(6)
-        imgDice.setImageDrawable(getDrawable(dice.roll()))
+        val imgRes = dice.roll()
+        // fade in and fade out image
+        imgDice.animate().apply {
+            duration = 200
+            // alpha(0f)
+            rotationYBy(360f)
+            withEndAction {
+                imgDice.setImageResource(imgRes)
+                // alpha(1f)
+            }
+        }.start()
     }
 }
 
 class Dice(private val numSides: Int) {
     fun roll(): Int {
-        return R.drawable.dice_6
+        return when ((1..numSides).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> 0
+        }
     }
 }
--- app/src/test/java/com/digipod/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/digipod/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.digipod.diceroller
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,17 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun multiply_test_1() {
+        assertEquals(100, 10 * 10)
+    }
+
+    @Test
+    fun dice_roll_test() {
+        val dice = Dice(6)
+        val result = dice.roll()
+        assertNotEquals(0, result)
     }
 }
